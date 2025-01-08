462018adecfeadaa494c8c1bf94a277e6871dacf
Michelle <jiahu0724417@gmail.com>
2025-01-08 10:21:17 +0800

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+//import './index.css'
+import 'bootstrap'
+import App from './App.jsx'
+import './assets/all.scss'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
